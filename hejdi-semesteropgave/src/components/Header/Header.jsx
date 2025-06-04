0b2025871c420496bb8d1ba50f53c845f908d6ff
Hejdi Østergaard Nielsen <1000041081@mcdmstudent.dk>
2025-06-04 09:50:38 +0200

    update Header - rename Favorites

--- hejdi-semesteropgave/src/components/Header/Header.jsx
+++ hejdi-semesteropgave/src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import { NavLink } from "react-router-dom";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { IoClose } from "react-icons/io5";
 import { AiFillHome } from "react-icons/ai";
+import { FaHeart } from "react-icons/fa6";
 import styles from "./Header.module.css";
 
 export default function Header() {
@@ -17,13 +18,39 @@ export default function Header() {
         <NavLink to="/" className={styles.link}>
           <AiFillHome size={28} />
         </NavLink>
-        <NavLink to="/aboutcontactpage" className={styles.link}>
-        Om os
+        <NavLink to="/products" className={styles.link}>
+          Produkter
         </NavLink>
-        <NavLink to="/productlistpage" className={styles.link}>
-        Produkter</NavLink>
-
+        <NavLink to="/aboutcontactpage" className={styles.link}>
+          Om os
+        </NavLink>
+        <NavLink to="aboutcontactpage" className={styles.link}>
+          Kontakt os
+        </NavLink>
+        <NavLink to=""
       </nav>
+
+      <div className={styles.burgerIcon} onClick={() => setMenuOpen(true)}>
+        <RxHamburgerMenu size={28} />
+      </div>
+
+      <div className={`${styles.overlay} ${menuOpen ? styles.show : ""}`}>
+        <div className={styles.closeIcon} onClick={() => setMenuOpen(false)}>
+          <IoClose size={28} />
+        </div>
+
+        <nav className={styles.mobileNav}>
+            <NavLink to="/" className={styles.mobileLink} onClick={handleLinkClick} >
+            HJEM
+            </NavLink>
+            <NavLink to="/aboutcontactpage" className={styles.mobileLink} onClick={handleLinkClick} >
+            OM OS
+            </NavLink>
+
+
+
+        </nav>
+      </div>
     </header>
   );
 }
