7ff5e297d83ac316663b663c279592d069bf82c0
Hejdi Østergaard Nielsen <1000041081@mcdmstudent.dk>
2025-06-11 10:32:59 +0200

    update css

--- hejdi-semesteropgave/src/pages/Home/Home.jsx
+++ hejdi-semesteropgave/src/pages/Home/Home.jsx
@@ -1,16 +1,22 @@
 import TopRatedProducts from "../../components/TopRatedProducts/TopRatedProducts";
+import { Link } from "react-router-dom";
 import styles from "./Home.module.css";
 
 export default function Home({ addToCart, favorites, toggleFavorites }) {
   return (
     <div className={styles.homeContainer}>
-      <h1>Velkommen til min hjemmeside</h1>;
+      <h1>Velkommen til min hjemmeside</h1>
       <div>
         <TopRatedProducts
           favoriteIds={favorites}
           toggleFavorite={toggleFavorites}
           addToCart={addToCart}
         />
+        <div className={styles.goToProducts}>
+          <Link to="/products" className={styles.goToProductsBtn}>
+            Se alle produkter
+          </Link>
+        </div>
       </div>
     </div>
   );
